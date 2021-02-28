8e11606b0e150c8b1158191fbb5120dfee8654fd
QUARK88 <72112478+QUARK88@users.noreply.github.com>
2021-02-28 18:44:31 -0500

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -3,99 +3,99 @@ ideologies = [
         "name": "Paternalistic Conservatism",
         "stats": {
             "econ": 30,
-            "dipl": 25,
+            "dipl": 75,
             "govt": 20,
-            "scty": 45
+            "scty": 55
         }
     },
     {
         "name": "Inhibition",
         "stats": {
             "econ": 0,
-            "dipl": 0,
+            "dipl": 100,
             "govt": 0,
-            "scty": 0
+            "scty": 100
         }
     },
     {
         "name": "Libertarian Socialism",
         "stats": {
             "econ": 40,
-            "dipl": 100,
+            "dipl": 0,
             "govt": 60,
-            "scty": 80
+            "scty": 20
         }
     },
     {
         "name": "Libertarian Market Socialism",
         "stats": {
             "econ": 70,
-            "dipl": 95,
+            "dipl": 5,
             "govt": 65,
-            "scty": 85
+            "scty": 15
         }
     },
     {
         "name": "Anarcho Capitalism",
         "stats": {
             "econ": 95,
-            "dipl": 0,
+            "dipl": 100,
             "govt": 60,
-            "scty": 100
+            "scty": 0
         }
     },
     {
         "name": "Market Anarchism",
         "stats": {
             "econ": 80,
-            "dipl": 85,
+            "dipl": 15,
             "govt": 70,
-            "scty": 100
+            "scty": 0
         }
     },
     {
         "name": "FALGSC",
         "stats": {
             "econ": 70,
-            "dipl": 95,
+            "dipl": 5,
             "govt": 95,
-            "scty": 75
+            "scty": 25
         }
     },
     {
         "name": "Fordism",
         "stats": {
             "econ": 90,
-            "dipl": 40,
+            "dipl": 60,
             "govt": 100,
-            "scty": 60
+            "scty": 40
         }
     },
     {
         "name": "Quarkism",
         "stats": {
             "econ": 20,
-            "dipl": 60,
+            "dipl": 40,
             "govt": 70,
-            "scty": 45
+            "scty": 55
         }
     },
     {
         "name": "Primitivism",
         "stats": {
             "econ": 10,
-            "dipl": 80,
+            "dipl": 20,
             "govt": 15,
-            "scty": 90
+            "scty": 10
         }
     },
     {
         "name": "Capacitationism",
         "stats": {
             "econ": 100,
-            "dipl": 100,
+            "dipl": 0,
             "govt": 100,
-            "scty": 100
+            "scty": 0
         }
     }
 ];
